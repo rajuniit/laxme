6be3a9c1ab5e442924ba69e40a431406b86d62a7
Raju Mazumder <rajuniit@gmail.com>
2017-02-17 08:32:38 +0000

    added deploy config

---
+++ config/deploy.rb
@@ -0,0 +1,55 @@
+# config valid only for current version of Capistrano
+lock '3.6.1'
+
+set :application, 'laxme'
+set :repo_url, 'git@github.com:rajuniit/laxme.git'
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/var/www/laxme'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, 'config/database.yml', 'config/secrets.yml'
+
+# Default value for linked_dirs is []
+# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/puma.rb')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :delayed_job_workers, 2
+set :delayed_job_prefix, 'reports'
+set :delayed_job_roles, [:app, :background]
+set :delayed_job_bin_path, "bin"
+# Puma:
+set :puma_conf, "#{shared_path}/config/puma.rb"
+ 
+namespace :deploy do
+  before 'check:linked_files', 'puma:config'
+  before 'check:linked_files', 'puma:nginx_config'
+  after 'puma:smart_restart', 'nginx:restart'
+
+ after 'deploy:published', 'delayed_job:restart' do
+    invoke 'delayed_job:restart'
+end
+end
